4e95f59b0106780495274d46d9778abec88798db
baranberkay96 <baranberkay96@gmail.com>
2019-01-23 18:15:22 +0300

    init commit

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    url('snippets/', views.SnippetList.as_view()),
+    url('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
